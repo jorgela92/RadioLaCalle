787287c1a5488018d97bfab427534618b1044aab
Jorge Lapeña Anton <equipo-santander@hcd.es>
2019-06-19 18:20:43 +0200

    Auctualizate View

--- src/app/components/listprogram/listprogram.component.ts
+++ src/app/components/listprogram/listprogram.component.ts
@@ -2,11 +2,29 @@ import { Component, OnInit } from '@angular/core';
 import {RadioProgram} from '../../app.component';
 import {DatabaseService} from '../../services/database.service';
 import {Router} from '@angular/router';
+import {trigger, state, style, animate, transition, query, stagger} from '@angular/animations';
 
 @Component({
   selector: 'app-listprogram',
   templateUrl: './listprogram.component.html',
-  styleUrls: ['./listprogram.component.css']
+  styleUrls: ['./listprogram.component.css'],
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger('50ms',
+              animate(
+                '550ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ], { optional: true }),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {optional: true})
+      ])
+    ])
+  ]
 })
 
 export class ListprogramComponent implements OnInit {
